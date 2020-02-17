00c126794c3b57b9de276d6a96cc0f2787daaeb6
권태일 <kwontaeil@gwontaeil-ui-MacBookPro.local>
2020-02-17 15:23:32 +0900

    front ToDoWeather Chrome App

---
+++ weather.js
@@ -0,0 +1,54 @@
+const weather = document.querySelector('.js-weather')
+const API_KEY = "8fbd517a3d361799c173f377fbef346b"
+const COORDS = 'coords'
+
+function getWeather(lat, lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+    .then(function(response){
+        return response.json()
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`
+    })
+};
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj))
+}
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude)
+}
+
+function handleGeoError(){
+    console.log("Cant")
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError)
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    } else {
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude)
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
